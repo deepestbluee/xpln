878f97566db60dc53eb428b995f1cec09c98cd0d
deepestbluee <umutcangulmez@gmail.com>
2019-01-19 15:04:05 +0300

    assignment + 4 islem

--- target_tokens1n.s
+++ target_tokens1n.s
@@ -130,3 +130,382 @@ s.s $f0,A
 li $v0,10
 syscall
 .end main
+
+.data
+
+B: .float 0.0
+C: .float 0.0
+D: .float 0.0
+E: .float 0.0
+F: .float 0.0
+t391: .float 0.0
+t392: .float 0.0
+t393: .float 0.0
+t394: .float 0.0
+t395: .float 0.0
+A: .float 0.0
+zzeerroo: .float 0.0
+
+.text
+
+main:
+li.s $f0,5.0
+s.s $f0,B
+#MIPs termination protocol:
+li $v0,10
+syscall
+.end main
+
+.data
+
+B: .float 0.0
+C: .float 0.0
+D: .float 0.0
+E: .float 0.0
+F: .float 0.0
+t390: .float 0.0
+t391: .float 0.0
+t392: .float 0.0
+t393: .float 0.0
+t394: .float 0.0
+A: .float 0.0
+zzeerroo: .float 0.0
+
+.text
+
+main:
+li.s $f0,5.0
+s.s $f0,B
+li.s $f0,2.0
+s.s $f0,C
+li.s $f0,10.0
+s.s $f0,D
+li.s $f0,10.0
+s.s $f0,E
+li.s $f0,20.0
+s.s $f0,F
+l.s $f0,C
+l.s $f2,D
+mul.s $f0,$f0,$f2
+s.s $f0,t390
+l.s $f0,E
+l.s $f2,F
+add.s $f0,$f0,$f2
+s.s $f0,t391
+l.s $f0,t391
+#converting to float
+li $t0,5
+mtc1 $t0,$f6
+cvt.s.w $f2,$f6
+#conversion done
+div.s $f0,$f0,$f2
+s.s $f0,t392
+l.s $f0,t390
+l.s $f2,t392
+sub.s $f0,$f0,$f2
+s.s $f0,t393
+l.s $f0,B
+l.s $f2,t393
+add.s $f0,$f0,$f2
+s.s $f0,t394
+l.s $f0,t394
+s.s $f0,A
+#MIPs termination protocol:
+li $v0,10
+syscall
+.end main
+
+.data
+
+B: .float 0.0
+C: .float 0.0
+D: .float 0.0
+E: .float 0.0
+F: .float 0.0
+t390: .float 0.0
+t391: .float 0.0
+t392: .float 0.0
+t393: .float 0.0
+t394: .float 0.0
+A: .float 0.0
+zzeerroo: .float 0.0
+
+.text
+
+main:
+li.s $f0,5.0
+s.s $f0,B
+li.s $f0,2.0
+s.s $f0,C
+li.s $f0,10.0
+s.s $f0,D
+li.s $f0,10.0
+s.s $f0,E
+li.s $f0,20.0
+s.s $f0,F
+l.s $f0,C
+l.s $f2,D
+mul.s $f0,$f0,$f2
+s.s $f0,t390
+l.s $f0,E
+l.s $f2,F
+add.s $f0,$f0,$f2
+s.s $f0,t391
+l.s $f0,t391
+#converting to float
+li $t0,5
+mtc1 $t0,$f6
+cvt.s.w $f2,$f6
+#conversion done
+div.s $f0,$f0,$f2
+s.s $f0,t392
+l.s $f0,t390
+l.s $f2,t392
+sub.s $f0,$f0,$f2
+s.s $f0,t393
+l.s $f0,B
+l.s $f2,t393
+add.s $f0,$f0,$f2
+s.s $f0,t394
+l.s $f0,t394
+s.s $f0,A
+#MIPs termination protocol:
+li $v0,10
+syscall
+.end main
+
+.data
+
+B: .float 0.0
+C: .float 0.0
+D: .float 0.0
+E: .float 0.0
+F: .float 0.0
+t396: .float 0.0
+t397: .float 0.0
+t398: .float 0.0
+t399: .float 0.0
+t400: .float 0.0
+A: .float 0.0
+zzeerroo: .float 0.0
+
+.text
+
+main:
+li.s $f0,5.0
+s.s $f0,B
+li.s $f0,2.0
+s.s $f0,C
+li.s $f0,10.0
+s.s $f0,D
+li.s $f0,10.0
+s.s $f0,E
+li.s $f0,20.0
+s.s $f0,F
+l.s $f0,C
+l.s $f2,D
+mul.s $f0,$f0,$f2
+s.s $f0,t396
+l.s $f0,E
+l.s $f2,F
+add.s $f0,$f0,$f2
+s.s $f0,t397
+l.s $f0,t397
+#converting to float
+li $t0,5
+mtc1 $t0,$f6
+cvt.s.w $f2,$f6
+#conversion done
+div.s $f0,$f0,$f2
+s.s $f0,t398
+l.s $f0,t396
+l.s $f2,t398
+sub.s $f0,$f0,$f2
+s.s $f0,t399
+l.s $f0,B
+l.s $f2,t399
+add.s $f0,$f0,$f2
+s.s $f0,t400
+l.s $f0,t400
+s.s $f0,A
+#MIPs termination protocol:
+li $v0,10
+syscall
+.end main
+
+.data
+
+B: .float 0.0
+C: .float 0.0
+D: .float 0.0
+E: .float 0.0
+F: .float 0.0
+t390: .float 0.0
+t391: .float 0.0
+t392: .float 0.0
+t393: .float 0.0
+t394: .float 0.0
+A: .float 0.0
+zzeerroo: .float 0.0
+
+.text
+
+main:
+li.s $f0,5.0
+s.s $f0,B
+li.s $f0,2.0
+s.s $f0,C
+li.s $f0,10.0
+s.s $f0,D
+li.s $f0,10.0
+s.s $f0,E
+li.s $f0,20.0
+s.s $f0,F
+l.s $f0,C
+l.s $f2,D
+mul.s $f0,$f0,$f2
+s.s $f0,t390
+l.s $f0,E
+l.s $f2,F
+add.s $f0,$f0,$f2
+s.s $f0,t391
+l.s $f0,t391
+#converting to float
+li $t0,5
+mtc1 $t0,$f6
+cvt.s.w $f2,$f6
+#conversion done
+div.s $f0,$f0,$f2
+s.s $f0,t392
+l.s $f0,t390
+l.s $f2,t392
+sub.s $f0,$f0,$f2
+s.s $f0,t393
+l.s $f0,B
+l.s $f2,t393
+add.s $f0,$f0,$f2
+s.s $f0,t394
+l.s $f0,t394
+s.s $f0,A
+#MIPs termination protocol:
+li $v0,10
+syscall
+.end main
+
+.data
+
+B: .float 0.0
+C: .float 0.0
+D: .float 0.0
+E: .float 0.0
+F: .float 0.0
+t390: .float 0.0
+t391: .float 0.0
+t392: .float 0.0
+t393: .float 0.0
+t394: .float 0.0
+A: .float 0.0
+zzeerroo: .float 0.0
+
+.text
+
+main:
+li.s $f0,5.0
+s.s $f0,B
+li.s $f0,2.0
+s.s $f0,C
+li.s $f0,10.0
+s.s $f0,D
+li.s $f0,10.0
+s.s $f0,E
+li.s $f0,20.0
+s.s $f0,F
+l.s $f0,C
+l.s $f2,D
+mul.s $f0,$f0,$f2
+s.s $f0,t390
+l.s $f0,E
+l.s $f2,F
+add.s $f0,$f0,$f2
+s.s $f0,t391
+l.s $f0,t391
+#converting to float
+li $t0,5
+mtc1 $t0,$f6
+cvt.s.w $f2,$f6
+#conversion done
+div.s $f0,$f0,$f2
+s.s $f0,t392
+l.s $f0,t390
+l.s $f2,t392
+sub.s $f0,$f0,$f2
+s.s $f0,t393
+l.s $f0,B
+l.s $f2,t393
+add.s $f0,$f0,$f2
+s.s $f0,t394
+l.s $f0,t394
+s.s $f0,A
+#MIPs termination protocol:
+li $v0,10
+syscall
+.end main
+
+.data
+
+B: .float 0.0
+C: .float 0.0
+D: .float 0.0
+E: .float 0.0
+F: .float 0.0
+t400: .float 0.0
+t401: .float 0.0
+t402: .float 0.0
+t403: .float 0.0
+t404: .float 0.0
+A: .float 0.0
+zzeerroo: .float 0.0
+
+.text
+
+main:
+li.s $f0,5.0
+s.s $f0,B
+li.s $f0,2.0
+s.s $f0,C
+li.s $f0,10.0
+s.s $f0,D
+li.s $f0,10.0
+s.s $f0,E
+li.s $f0,20.0
+s.s $f0,F
+l.s $f0,C
+l.s $f2,D
+mul.s $f0,$f0,$f2
+s.s $f0,t400
+l.s $f0,E
+l.s $f2,F
+add.s $f0,$f0,$f2
+s.s $f0,t401
+l.s $f0,t401
+#converting to float
+li $t0,5
+mtc1 $t0,$f6
+cvt.s.w $f2,$f6
+#conversion done
+div.s $f0,$f0,$f2
+s.s $f0,t402
+l.s $f0,t400
+l.s $f2,t402
+sub.s $f0,$f0,$f2
+s.s $f0,t403
+l.s $f0,B
+l.s $f2,t403
+add.s $f0,$f0,$f2
+s.s $f0,t404
+l.s $f0,t404
+s.s $f0,A
+#MIPs termination protocol:
+li $v0,10
+syscall
+.end main
--- target_tokens3n.s
+++ target_tokens3n.s
@@ -57,3 +57,55 @@ s.s $f0,A
 li $v0,10
 syscall
 .end main
+
+.data
+
+B: .float 0.0
+C: .float 0.0
+E: .float 0.0
+F: .float 0.0
+t395: .float 0.0
+t396: .float 0.0
+t397: .float 0.0
+t398: .float 0.0
+t399: .float 0.0
+A: .float 0.0
+zzeerroo: .float 0.0
+
+.text
+
+main:
+li.s $f0,10.0
+s.s $f0,B
+li.s $f0,12.0
+s.s $f0,C
+li.s $f0,14.0
+s.s $f0,E
+li.s $f0,16.2
+s.s $f0,F
+l.s $f0,C
+li.s $f2,78.45
+mul.s $f0,$f0,$f2
+s.s $f0,t395
+l.s $f0,E
+l.s $f2,F
+add.s $f0,$f0,$f2
+s.s $f0,t396
+l.s $f0,t396
+li.s $f2,12.5
+div.s $f0,$f0,$f2
+s.s $f0,t397
+l.s $f0,t395
+l.s $f2,t397
+sub.s $f0,$f0,$f2
+s.s $f0,t398
+l.s $f0,B
+l.s $f2,t398
+add.s $f0,$f0,$f2
+s.s $f0,t399
+l.s $f0,t399
+s.s $f0,A
+#MIPs termination protocol:
+li $v0,10
+syscall
+.end main
